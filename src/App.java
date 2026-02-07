663ab5740d3535d4ed06a4fdfafeed1ecddc5059
umer <syedumerahmedcode@gmail.com>
2026-02-07 07:00:16 +0500

    Feature: Implemented the soluton and tested it against a valid test case scenario.

--- src/App.java
+++ src/App.java
@@ -3,24 +3,28 @@ public class App {
         System.out.println("Hello, World!");
         /**
          * Given an integer array nums, find a subarray that has the largest product, and return the product.
-
-The test cases are generated so that the answer will fit in a 32-bit integer.
-
-Note that the product of an array with a single element is the value of that element.
-
- 
-
-Example 1:
-
-Input: nums = [2,3,-2,4]
-Output: 6
-Explanation: [2,3] has the largest product 6.
-Example 2:
-
-Input: nums = [-2,0,-1]
-Output: 0
-Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
- 
+        
+        The test cases are generated so that the answer will fit in a 32-bit integer.
+        
+        Note that the product of an array with a single element is the value of that element.
+        
+        
+        
+        Example 1:
+        
+        Input: nums = [2,3,-2,4]
+        Output: 6
+        Explanation: [2,3] has the largest product 6.
+        Example 2:
+        
+        Input: nums = [-2,0,-1]
+        Output: 0
+        Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+        
          */
+        int[] nums = { 2, 3, -2, 4 };
+        Solution solution = new Solution();
+        int result = solution.maxProduct(nums);
+        System.out.println("The maximum subarray product that we can get for the input given above is: "+result);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -4,6 +4,21 @@ public class Solution {
         /**
          * solved using dynamic programming
          */
+        if (nums.length == 0) {
+            return 0;
+        }
+        int min = nums[0];
+        int max = nums[0];
+        int result = max;
+
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i];
+            int temp = Math.max(curr, Math.max(max * curr, min * curr));
+            min = Math.min(curr, Math.min(max * curr, min * curr));
+            max = temp;
+            result = Math.max(result, max);
+        }
+        return result;
         
     }
 
